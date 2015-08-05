8942eb56193ec13d457574a5e356184d123f49ee
Robert Gallup <bob@robertgallup.com>
2015-08-05 06:35:53 -0700

    Removed some flotsam

--- LEDCube/LEDCube.ino
+++ LEDCube/LEDCube.ino
@@ -6,7 +6,7 @@
 // It is based on the original Radio Shack code, but with animation data in program memory
 // it allows many more animations
 //
-// By Robert Gallup (http://www.robertgallup.com, http://www.github.com/robertgallup)
+// Robert Gallup (http://www.robertgallup.com, http://www.github.com/robertgallup)
 //
 ////////////////////////////////////////////////////////////////////////////////////////
 
@@ -16,9 +16,6 @@
 const unsigned int animations = (sizeof(animation) / 2 / 4 / 16);
 
 void setup() {
-  
-  // Number of animations
-//  animations = (sizeof(animation) / 2 / 4 / 16);
   
   // Initialize pins 2-13 for output
   for(int p=2; p<=13; p++) pinMode(p, OUTPUT);
